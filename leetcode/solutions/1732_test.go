2c4b6761b614afb94db41bf31acd4fbcd445de29
rahulkookal <rahulkkodoth@gmail.com>
2025-03-09 17:06:41 +0000

    test cases for Largest altitude

--- leetcode/solutions/1732_test.go
+++ leetcode/solutions/1732_test.go
@@ -1,3 +1,36 @@
 package solutions
 
-//TODO
+import "testing"
+
+func TestLargestAltitude(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{
+			name:     "array with integers",
+			input:    []int{-5, 1, 5, 0, -7},
+			expected: 1,
+		},
+		{
+			name:     "empty array",
+			input:    []int{},
+			expected: 0,
+		},
+		{
+			name:     "hight zero",
+			input:    []int{0, -4, -7, -9, -10, -6, -3, -1},
+			expected: 0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := LargestAltitude(test.input)
+			if result != test.expected {
+				t.Errorf("Test failed. Result: %d expected %d", result, test.expected)
+			}
+		})
+	}
+}
