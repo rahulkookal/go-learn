d495d6da10da02e5c9df031343469b34fceaf45c
rahulkookal <rahulkkodoth@gmail.com>
2025-03-16 17:17:21 +0000

    Desc

--- leetcode/cmd/experiment.go
+++ leetcode/cmd/experiment.go
@@ -12,7 +12,7 @@ import (
 // experimentCmd represents the experiment command
 var experimentCmd = &cobra.Command{
 	Use:   "experiment",
-	Short: "Used to try out go code",
+	Short: "",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("experiment called")
